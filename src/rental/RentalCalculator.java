49f0406dd355d8731455e2fef8916e5d9adcf942
6r4n <granvillelintao@gmail.com>
2024-08-20 22:03:39 +0200

    #1 Make RentalCalculator more readable

--- src/rental/RentalCalculator.java
+++ src/rental/RentalCalculator.java
@@ -4,24 +4,36 @@ import movie.Movie;
 import movie.MovieRental;
 
 public class RentalCalculator {
+
     public double calculateAmount(Movie movie, MovieRental rental) {
-        double amount = 0;
         switch (movie.getCode()) {
             case REGULAR:
-                amount = 2;
-                if (rental.getDays() > 2) {
-                    amount += (rental.getDays() - 2) * 1.5;
-                }
-                break;
+                return calculateRegularAmount(rental);
             case NEW:
-                amount = rental.getDays() * 3;
-                break;
+                return calculateNewReleaseAmount(rental);
             case CHILDRENS:
-                amount = 1.5;
-                if (rental.getDays() > 3) {
-                    amount += (rental.getDays() - 3) * 1.5;
-                }
-                break;
+                return calculateChildrenAmount(rental);
+            default:
+                throw new IllegalArgumentException("Unknown movie code: " + movie.getCode());
+        }
+    }
+
+    private double calculateRegularAmount(MovieRental rental) {
+        double amount = 2;
+        if (rental.getDays() > 2) {
+            amount += (rental.getDays() - 2) * 1.5;
+        }
+        return amount;
+    }
+
+    private double calculateNewReleaseAmount(MovieRental rental) {
+        return rental.getDays() * 3;
+    }
+
+    private double calculateChildrenAmount(MovieRental rental) {
+        double amount = 1.5;
+        if (rental.getDays() > 3) {
+            amount += (rental.getDays() - 3) * 1.5;
         }
         return amount;
     }
